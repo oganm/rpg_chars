43ec46fc3c32950d9b852335eee419dcd93d6eee
OganM <ogan.mancarcii@gmail.com>
2017-06-20 23:41:53 -0700

    tim update

--- Tim.R
+++ Tim.R
@@ -1,7 +1,38 @@
-source('importTim.R')
+library(import5eChar)
+library(diceSyntax)
 
-# initiative --------------
-init()
+char = importCharacter('Tim_Fighter')
+
+# by default the name getOption('defaultCharacter') returns 'char'. If another name is used use options(defaultCharacter = 'whatever') to set or manually
+# specify char when running functions
+
+timAttack = function(adv = 0,
+                     sharpShoot = F,
+                     attackStat = 'Dex',
+                     damageDice = '1d6',
+                     proficient = TRUE,
+                     modToHit = 2, # tim's archery
+                     modToDamage = 0,
+                     useAmmo = TRUE,
+                     ammo = 'bolt',
+                     vocal = TRUE,
+                     char = getOption('defaultCharacter')){
+    
+    attack(adv = adv,
+           sharpShoot = sharpShoot,
+           attackStat = attackStat,
+           damageDice = damageDice,
+           proficient = proficient,
+           modToHit = modToHit, # tim's archery
+           modToDamage = modToDamage,
+           useAmmo = useAmmo,
+           ammo = ammo,
+           vocal = vocal,
+           char = char)
+    
+}
+
+
 # ability scores ----------
 char$abilityMods
 # saves -------------
@@ -12,62 +43,79 @@ save('Chr')
 save('Int')
 save('Wis')
 
+abilityCheck('Str')
+abilityCheck('Dex')
+abilityCheck('Con')
+abilityCheck('Chr')
+abilityCheck('Int')
+abilityCheck('Wis')
+
 # checks -----------
 quickCheck()
 
-skillCheck(athletics)
+skillCheck(athletics) + char$abilityMods['Str']
 
-skillCheck(acrobatics)
-skillCheck(sleight)
-skillCheck(stealth)
+skillCheck(acrobatics) + char$abilityMods['Dex']
+skillCheck(sleight) + char$abilityMods['Dex']
+skillCheck(stealth) + char$abilityMods['Dex']
 
-skillCheck(arcana)
-skillCheck(history)
-skillCheck(investigation)
-skillCheck(nature)
-skillCheck(religion)
+skillCheck(arcana) + char$abilityMods['Int']
+skillCheck(history) + char$abilityMods['Int']
+skillCheck(investigation) + char$abilityMods['Int']
+skillCheck(nature) + char$abilityMods['Int']
+skillCheck(religion) + char$abilityMods['Int']
 
-skillCheck(animal)
-skillCheck(insight)
-skillCheck(medicine)
-skillCheck(perception)
-skillCheck(survival)
+skillCheck(animal) + char$abilityMods['Wis']
+skillCheck(insight) + char$abilityMods['Wis']
+skillCheck(medicine) + char$abilityMods['Wis']
+skillCheck(perception) + char$abilityMods['Wis']
+skillCheck(survival) + char$abilityMods['Wis']
 
-skillCheck(deception)
-skillCheck(intimidation)
-skillCheck(persuasion)
-skillCheck(performance)
+skillCheck(deception) + char$abilityMods['Chr']
+skillCheck(intimidation) + char$abilityMods['Chr']
+skillCheck(persuasion) + char$abilityMods['Chr']
+skillCheck(performance) + char$abilityMods['Chr']
 
 
 
 # health ------------
-char$maxHealth
-
-r(r1d20) +3
-
-r(r1d20)
-
-# attack --------------
-char$bolt = 49
-char$arrow = 20
-
-60 -1-1-1 -1-1-1 -1 -1 -1
-attack()
-attack(1)
-attack(-1)
-
-
-attack(ammo = 'arrow')
-attack(1,ammo = 'arrow')
-attack(-1,ammo = 'arrow')
-
-attack(sharpShoot = TRUE)
-attack(1,sharpShoot = TRUE)
-attack(-1,sharpShoot = TRUE)
+char$maxHealth -15 +12 
 
 # second wind
 # d10+figter level
 r(r1d10) + 2
+
+# initiative --------------
+init()
+
+# attack --------------
+char$bolt = 31
+char$arrow = 20
+
+# hand crossbow
+timAttack() 
+timAttack(1)
+timAttack(-1)
+
+timAttack(sharpShoot = TRUE)
+timAttack(1,sharpShoot = TRUE)
+timAttack(-1,sharpShoot = TRUE)
+
+# dagger
+timAttack(damageDice= '1d4',useAmmo = FALSE,modToHit = 0)
+timAttack(1,damageDice= '1d4',useAmmo = FALSE,modToHit = 0)
+timAttack(-1,damageDice= '1d4',useAmmo = FALSE,modToHit = 0)
+
+# longbow
+timAttack(damageDice = '1d8',ammo = 'arrow')
+timAttack(1,damageDice = '1d8',ammo = 'arrow')
+timAttack(-1,damageDice = '1d8',ammo = 'arrow')
+
+timAttack(damageDice = '1d8',ammo = 'arrow',sharpShoot = TRUE)
+timAttack(1,damageDice = '1d8',ammo = 'arrow',sharpShoot = TRUE)
+timAttack(-1,damageDice = '1d8',ammo = 'arrow',sharpShoot = TRUE)
+
+
 
 r(r1d20)
 
@@ -78,11 +126,32 @@ r(r1d20)
 ###### 2 gold
 ##### 10 silver
 
+
 # warlock
 ###### 2 gold
 # weird bone thingy
 
-# briar glen 
+# 2 chainmail
+# 4 daggers
+# 2 longswords 
+# 4 gold
+
+# 15 sp 7 cp 
+# bone dice
+# rat statue metal
+# demonically carved stuff
+# friendship bracelets
+# doll with one eye
+# spoon
+# weird mage
+# demonic carved tokens
+# ring W
+# smoking pipe N
+# keyring
+# battle maps
+# 
+
+# briar glen
 
 10/4
 
@@ -91,4 +160,3 @@ r(r1d20)
 (20 +30 +2 +2)/4
 
 13.5 + 0.25
-
