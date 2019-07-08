30e4e12507b5de246813bd974ea3e23d5e52c822
Ogan Mancarci <ogan.mancarci@gmail.com>
2019-07-08 12:59:48 -0700

    new char

---
+++ String.R
@@ -0,0 +1,31 @@
+15 - 
+    1 -
+    5 -
+    2 -
+    1 +
+    7.4/6 +
+    15/6
+ 
+
+
+
+"
+goblet 20 gold
+10 gold
+
+
+ball bearings
+mirror
+• a Backpack
+• a Crowbar
+• a Hammer
+• 10 pitons
+• 10 torches
+• a Tinderbox
+• 10 days of Rations
+• a Waterskin
+• 50 feet of Hempen rope
+
+Manacles
+bell
+"
