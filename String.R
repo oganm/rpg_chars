2828dea2ed846a1c0fcde68017d0eb55a1dc307e
Ogan Mancarci <ogan.mancarci@gmail.com>
2019-11-22 20:54:53 -0800

    string update

--- String.R
+++ String.R
@@ -4,12 +4,49 @@
     2 -
     1 +
     7.4/6 +
-    15/6
- 
+    15/6 +
+    2000/4 - 60 - 100 + 7 - 1 -2 - .05 * 20 -2
 
 
 
 "
+
+-1 wisdom
+
+
+Villark
+
+
+
+How to screw up wishing
+Make friends by enchanting
+
+
+scrolls
+========
+eyebite
+remove curse
+
+
+7.5'
+
+entrence is at the southern part of the outskirts. trees dwindle quary
+use the wand truesight. mirage arcane. Traxigar is dead.
+
+sun appeared over eltarel. site of pilgrimage.
+
+
+50 feet rope
+pitons x 20 
+crowbar
+traveler's clothes
+
+hat of wizardry cast 1 wizard cantrip per day for a DC10 arcana check
+
+candle of the deep, cloak of the manta ray
+
+
+
 The stuff you found in the rubble: a damaged book written in a language you don't know,
 a mortar and pestle, and the rotten remains of a walking stick 
 with multiple things tied onto the top: a broken bone, tied up pouch, more rotten wood, 
@@ -23,8 +60,7 @@ vakaki
 ritual sacrifice stuff
 religious text
 
--10 lay on hands
--1 lvl 1 spell slots
+
 
 goblet 20 gold
 10 gold
@@ -57,21 +93,22 @@ plusplus = poisinous Plan"
 
 "
 21 = block + ? + in +  sacrifice                 bar access
-20 = Mix + air + in + sacrifice                          unkown
-28 = ? + moss + water + sacrifice                 healing
-32 = mix + ? + ? + out + majorSac                 better healing
+20 = Mix + air + in + sacrifice                  unkown
+28 = ? + moss + water + sacrifice                healing
+32 = mix + ? + ? + out + majorSac                better healing
 33 = mix + moss + ? + in + majorSac
 ? = in + poison + out + air                      commune with dead
 8 = out + fire                                   burning hands
 ? = mix + poison + out + ?                       toxic liquid
-14 = mix + water + ? (max 8, min 5)                            water bending
-25 = ? + mix + fire + ? + majorSac               unleash wrath of the bound one
+14 = mix + water + ? (max 8, min 5)              water bending
+25 = air + mix + fire + out + majorSac           unleash wrath of the bound one
 42 = ? + fire + in + earth  + ? + ?              
 ? = out + water + in + ? + majorSac
 
 
 all <= 12
 block, in, out,  mix <= 4
+out <= 3
 rest >= 5
 
 5 <= fire <= 7
