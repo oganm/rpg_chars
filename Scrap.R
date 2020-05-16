243e8ed34293038750575d7add74a3e2d3cab05c
Ogan Mancarci <ogan.mancarci@gmail.com>
2020-05-15 18:26:20 -0700

    scrap inventory

---
+++ Scrap.R
@@ -0,0 +1,39 @@
+15 +  # background gp
+    25 + # sell 2 light crossbows
+    12.5 + # sell the last crossbow :(
+    2.5 - # sell 10 days of rations
+    2 -# whip
+    10 -  # shield
+    15  - # forgery kit
+    20 -# smith's tools
+    5 # herbalism kit
+
+
+8*15
+
+"
+Scale mail
+
+backpack
+a crowbar
+a hammer
+10 pitons
+10 torches
+a tinderbox
+waterskin
+50 feet of hempen rope
+
+20 bolts
+
+set of dark common clothes
+hood
+belt pouch
+
+forgery kit
+thieves tools
+smiths tools
+herbalism kit
+
+whip infused +1
+shield infused +1
+"
