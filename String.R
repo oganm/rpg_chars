24f42dacce4bff4768506aa1c93b474aed58934a
Ogan Mancarci <ogan.mancarci@gmail.com>
2020-05-15 17:00:57 -0700

    bump

--- String.R
+++ String.R
@@ -5,13 +5,110 @@
     1 +
     7.4/6 +
     15/6 +
-    2000/4 - 60 - 100 + 7 - 1 -2 - .05 * 20 -2
+    2000/4 - 60 - 100 + 7 - 1 -2 - .05 * 20 -2 +250/6 + 78.33 + 41 + 
+    40/5 - 5 -10 -20 + 2000/4 + 500/4 + 
+    (500+25+25)/5 + # art stuff
+    350/5 +  # components
+    2500/5 -
+    1500 -# plate armor
+    50 -  # base weeks
+    1 - 25 - 200 + 10/5 +
+    2150/5 + # cloak and staff and gems
+    240  + 60 
+
+
+# HP
+
+# weight
+
+16*15 -
+    65 -# plate mail 
+    20 - # caltrops
+    20 - # ball bearings
+    1 - # driftglobe
+    3 -  # healer's kit
+    10 - # rope
+    5 - # crowbar
+    4 -  # clothes 
+    10 - # chain
+    1 - # lock
+    5 - # backpack
+    1 - # mess kit
+    1 - # tinderbox
+    10 -# torches
+    5 - # waterskim
+    5 - # pitons
+    6 - # manacles
+    6 - # shield
+    3 - # scimitar
+    20 -  # javelins
+    .5 - # mirror
+    5 - # Block and Tackle
+    12 - # climber's kit
+    1.5 - # potion of healing
+    0 
 
 
 
 "
+2d10 5d8 hit die
 
--1 wisdom
+doppleganger name adkalskdalkd
+
+xorlarrin house draac xorlarrin
+find wood elf scout for Emerald Enclave
+find high elf scout for Lord's allience
+
+hidden lord ===========================
+---
+
+
+Shaerra wilzimih elf
+
+book of songs and biographies from myke
+
+3 potion of healing 2d4+2
+
+boots of speed =================================
+While you wear these boots, you can use a Bonus Action and click the boots' heels
+together. If you do, the boots double your walking speed, and any creature that
+makes an opportunity Attack against you has disadvantage on the Attack roll. If 
+you click your heels together again, you end the effect.
+
+When the boots' property has been used for a total of 10 minutes, the magic 
+ceases to function until you finish a Long Rest.
+
+goggles of night
+
+shield of the hidden lord ... action to cast fireball wall of fire 2 charges DC 21 no concentration
+fire res cold res
+
+potion of frost giant strength
+
+
+10 cltrops
+10 ball bearings
+
+cloak of manta ray swim speed 60
+
+
+potion of fire resistance
+candle
+whistle in darkness it casts animste dead one every 7 day
+
+This small sphere of thick glass weighs 1 pound. If you are within 60 feet of it, you can speak its command word and cause it to emanate the light or daylight spell. Once used, the daylight effect can't be used again until the next dawn.
+You can speak another command word as an action to make the illuminated globe rise into the air and float no more than 5 feet off the ground. The globe hovers in this way until you or another creature grasps it. If you move more than 60 feet from the hovering globe, it follows you until it is within 60 feet of you. It takes the shortest route to do so. If prevented from moving, the globe sinks gently to the ground and becomes inactive, and its light winks out.
+
+javelin of lightning  4d6 120 feet line lightning plus javelin
+
+bagpipe: disadvantage on being charmed
+
+driftglobe
+
+healer's kit
+
+necklace for sick dwarf husband, thorum. dwarfs ashes
+soul coin
 
 
 Villark
@@ -37,6 +134,7 @@ sun appeared over eltarel. site of pilgrimage.
 
 
 50 feet rope
+climber's kit
 pitons x 20 
 crowbar
 traveler's clothes
@@ -52,7 +150,6 @@ a mortar and pestle, and the rotten remains of a walking stick
 with multiple things tied onto the top: a broken bone, tied up pouch, more rotten wood, 
 
 
-1 10 gold gemstone
 
 
 vakai
@@ -60,14 +157,13 @@ vakaki
 ritual sacrifice stuff
 religious text
 
+10 feet chain
+lock
 
 
-goblet 20 gold
-10 gold
-
-
-ball bearings
 mirror
+Block and Tackle
+
 • a Backpack
 • a Bedroll
 • a Mess kit
